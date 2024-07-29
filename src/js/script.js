39181ca4712e6e869b95d1df7df6916491e44b6a
Atanas Kisyov <kisyov92@gmail.com>
2024-07-29 17:39:28 +0200

    first playable version without win message

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 const fieldElm = document.querySelector(".field");
+const gameOverWindowElm = document.querySelector(".game-over");
+const restartBtnElm = document.querySelector(".restart-btn");
 
 let rows = 10;
 let columns = 10;
-let bombs = 20;
+let bombs = 10;
 
 class Game {
   constructor(rows, cols, bombs) {
@@ -46,7 +48,6 @@ class Game {
       if (i < 0 || i > 9) continue;
       for (let j = startCol; j < startCol + 3; j++) {
         if (j < 0 || j > 9) continue;
-
         if (this.field[i][j] === "*") continue;
         this.field[i][j]++;
       }
@@ -59,11 +60,46 @@ class Game {
       rowElm.classList = "row";
       for (let j = 0; j < this.rows; j++) {
         const squareElm = document.createElement("div");
-        squareElm.classList = "sqare";
-        squareElm.textContent = this.field[i][j];
+        squareElm.classList = "square";
+        const squereContentElm = document.createElement("div");
+        squereContentElm.textContent = this.field[i][j];
+        squereContentElm.classList.add("square-content", "hidden");
+        squereContentElm.dataset.content = this.field[i][j];
+        rowElm.dataset.row = i;
+        squareElm.dataset.col = j;
+        squareElm.appendChild(squereContentElm);
         rowElm.appendChild(squareElm);
       }
       fieldElm.appendChild(rowElm);
+    }
+  }
+
+  openSqare(row, col) {
+    const fieldSquare = fieldElm
+      .querySelector(`[data-row="${row}"]`)
+      .querySelector(`[data-col="${col}"]`);
+    const fieldSquareContentElm = fieldSquare.querySelector(".square-content");
+    if (![...fieldSquareContentElm.classList].includes("hidden")) return;
+
+    fieldSquareContentElm.classList.remove("hidden");
+
+    if (fieldSquareContentElm.dataset.content === "*") {
+      fieldSquareContentElm.classList.add("bomb");
+      fieldElm.classList.add("disable-clicks");
+
+      setTimeout(() => {
+        gameOverWindowElm.classList.remove("hidden");
+      }, 1000);
+    }
+
+    if (fieldSquareContentElm.dataset.content === "null") {
+      for (let i = row - 1; i < row + 2; i++) {
+        if (i < 0 || i > 9) continue;
+        for (let j = col - 1; j < col + 2; j++) {
+          if (j < 0 || j > 9) continue;
+          this.openSqare(i, j);
+        }
+      }
     }
   }
 }
@@ -72,5 +108,20 @@ function randomNumBetween(minNum, maxNum) {
   return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 }
 
-const gameInstance = new Game(rows, columns, bombs);
-// console.log(gameInstance.field);
+fieldElm.addEventListener("click", (e) => {
+  if (!(e.target.classList.value === "square")) return;
+  const squareContentElm = e.target.querySelector("div");
+
+  const row = squareContentElm.parentElement.parentElement.dataset.row;
+  const col = squareContentElm.parentElement.dataset.col;
+  gameInstance.openSqare(+row, +col);
+});
+
+restartBtnElm.addEventListener("click", () => {
+  fieldElm.innerHTML = "";
+  gameInstance = new Game(rows, columns, bombs);
+  gameOverWindowElm.classList.add("hidden");
+  fieldElm.classList.remove("disable-clicks");
+});
+
+let gameInstance = new Game(rows, columns, bombs);
